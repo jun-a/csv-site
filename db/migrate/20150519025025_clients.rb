1cb2379323341312a38c5a478325f37f35f2906d
aramaki.jun <junradio910@gmail.com>
2015-05-19 12:52:01 +0900

    clientテーブルの作成

--- db/migrate/20150519025025_clients.rb
+++ db/migrate/20150519025025_clients.rb
@@ -1,4 +1,15 @@
 class Clients < ActiveRecord::Migration
   def change
+    create_table  :clients do  |t|
+      t.string  :name
+      t.string  :address
+      t.string  :tel_number
+      t.string  :mail
+      t.string  :client_president  #クライアントの社長名  
+      t.string  :client_manager #クライアントの担当者
+      t.string  :contract_status  #リジョブとの契約関係
+
+      t.timestanps  null: false
+    end
   end
 end
