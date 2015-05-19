74fdd0254759c85e8955608b8633a9b2e3746d51
aramaki.jun <junradio910@gmail.com>
2015-05-19 12:46:03 +0900

    active_report table作成

--- db/migrate/20150519025007_active_reports.rb
+++ db/migrate/20150519025007_active_reports.rb
@@ -1,4 +1,17 @@
 class ActiveReports < ActiveRecord::Migration
   def change
+    create_table  :active_reports do  |t|
+      t.string  :title
+      t.string  :content
+      t.string  :comment
+      t.date  :publish_start_date
+      t.date  :publish_end_date
+      t.references  :category
+      t.references  :client
+      t.references  :client_user
+      #t.references imageがここに入るかも  
+
+      t.timestanps   null: false
+    end
   end
 end
